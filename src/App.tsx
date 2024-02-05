5ab45004a51253b83b7edb83e054d3990bf7f9a2
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-02-06 01:08:54 +0300

    App.tsx changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * The main component of the application.
+ * Renders the navigation bar and sets up the routing for different pages.
+ *
+ * @returns The rendered App component.
+ */
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import User from './pages/User';
+import Homepage from './pages/Homepage';
+import RepositoryPage from './pages/RepositoryPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/:username" element={<User />} />
+          <Route path="/:username/repository" element={<RepositoryPage />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
