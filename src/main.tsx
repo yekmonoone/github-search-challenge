c02458f9b53de56871038c36e9741c325b7cfb8e
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-02-06 01:10:04 +0300

    main.tsx changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+/**
+ * Entry point of the application.
+ * Renders the root component wrapped in React.StrictMode and BrowserRouter.
+ * @returns {void}
+ */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>  
   </React.StrictMode>,
 )
