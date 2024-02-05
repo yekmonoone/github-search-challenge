df21a3d5adfecd880154e903ce5d4f70e94865f4
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-02-06 00:42:41 +0300

    getUserRepository api added

---
+++ src/api/getUserRepository.ts
@@ -0,0 +1,37 @@
+import Repository from "../interfaces/Repository";
+
+
+//documentation of the codes below
+
+const getUserRepository = async (username: string): Promise<Repository[]> => {
+    const headers = {
+        Authorization: "Bearer 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",
+    };
+
+    const res = await fetch(`https://api.github.com/users/${username}/repos`, { headers });
+
+    if (!res.ok) {
+        throw new Error(`Failed to fetch user ${username}: ${res.status}`);
+    }
+
+    const data = await res.json();
+    
+    const repositories : Repository[] = data?.map((repository: any) => ({
+        id: repository.id,
+        name : repository.name,
+        description : repository.description,
+        language : repository.language,
+        stargazers_count : repository.stargazers_count,
+        forks_count : repository.forks_count,
+        html_url : repository.html_url,
+        owner : {
+            id : repository.owner.id,
+            login : repository.owner.login
+        },
+        updated_at : repository.updated_at
+    }))
+    
+    return repositories;
+};
+
+export default getUserRepository;
