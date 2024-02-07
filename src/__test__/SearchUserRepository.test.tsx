a9adbfb800c4f3afc9a7fb78ec295a1fcb8e708f
ebhazarcodes <erdoganbaranhazar@gmail.com>
2024-02-07 15:02:54 +0300

    searchUserRepository test added

---
+++ src/__test__/SearchUserRepository.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import SearchUserRepository from '../components/SearchUserRepository/SearchUserRepository';
+import '@testing-library/jest-dom';
+
+
+describe('SearchUserRepository', () => {
+
+
+  test('renders search input and language select', () => {
+    render(<SearchUserRepository username="testuser" />);
+    
+    const searchInput = screen.getByLabelText('Search Repositories:');
+    const languageSelect = screen.getByLabelText('Select Language:');
+    
+    expect(searchInput).toBeInTheDocument();
+    expect(languageSelect).toBeInTheDocument();
+  });
+
+  
+
+});
